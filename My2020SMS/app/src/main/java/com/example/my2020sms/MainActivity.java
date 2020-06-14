b8c3cac1e1898415d5552f7c750b602031971fac
Satrio H Wicaksono <petapacode@gmail.com>
2020-06-14 19:57:53 +0700

    MainActivity View
    # remove ActionBar from all Activity
    # background color

--- My2020SMS/app/src/main/java/com/example/my2020sms/MainActivity.java
+++ My2020SMS/app/src/main/java/com/example/my2020sms/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.my2020sms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -16,36 +21,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startBtn = (Button) findViewById(R.id.button);
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        (findViewById(R.id.btnMessageSetting)).setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                sendSMS();
+                Intent intent = new Intent(MainActivity.this, MessageManagement.class);
+                MainActivity.this.startActivity(intent);
             }
         });
-    }
-
-    protected void sendSMS() {
-        Log.i("Send SMS", "");
-        Intent smsIntent = new Intent(Intent.ACTION_VIEW);
-
-        smsIntent.setData(Uri.parse("smsto:"));
-        smsIntent.setType("vnd.android-dir/mms-sms");
-        smsIntent.putExtra("address"  , new String ("01234"));
-        smsIntent.putExtra("sms_body"  , "Test ");
-
-        try {
-            startActivity(smsIntent);
-            finish();
-            Log.i("Finished sending SMS...", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this,
-                    "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
-        }
     }
 
 }
